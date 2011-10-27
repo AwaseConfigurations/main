35ad6f5959a7fe8171eb3f489120d0597169944d
Your Name <you@example.com>
2011-10-27 08:33:13 +0000

    added preseed, edited cobbler related configs

--- cobbler/cobbler_add_systems_5005.sh
+++
@@ -1,6 +0,0 @@
-#5005
-i=0
-for mac in aa:aa:aa:c0:8a:7f aa:aa:aa:c1:11:03 aa:aa:aa:c0:88:49 aa:aa:aa:c1:0a:83 aa:aa:aa:c0:88:fc aa:aa:aa:c0:97:e7 aa:aa:aa:c1:0a:85 aa:aa:aa:c1:09:a6 aa:aa:aa:c0:8a:7b aa:aa:aa:ba:7a:6b aa:aa:aa:c0:8e:ba aa:aa:aa:c0:8d:e0 aa:aa:aa:c1:09:9c aa:aa:aa:c1:09:56 aa:aa:aa:ba:77:e4 aa:aa:aa:c1:08:56 aa:aa:aa:c1:09:ae aa:aa:aa:ba:78:0b aa:aa:aa:c1:0a:42 aa:aa:aa:c2:8b:b5 aa:aa:aa:c0:8a:d0 aa:aa:aa:c2:8c:cd aa:aa:aa:c0:88:62 aa:aa:aa:c1:09:cb aa:aa:aa:c1:0a:87;
-do
-  sudo cobbler system add --name=$mac --profile=ubuntu-alternate-i386 --mac=$mac --hostname=host$((i++))
-done
---
+++ cobbler/cobbler_remove_systems.sh
@@ -0,0 +1,5 @@
+i=1
+for mac in 78:ac:c0:c0:8a:7f 78:ac:c0:c1:11:03 78:ac:c0:c0:88:49 78:ac:c0:c1:0a:83 78:ac:c0:c0:88:fc 78:ac:c0:c0:97:e7 78:ac:c0:c1:0a:85 78:ac:c0:c1:09:a6 78:ac:c0:c0:8a:7b 78:ac:c0:ba:7a:6b 78:ac:c0:c0:8e:ba 78:ac:c0:c0:8d:e0 78:ac:c0:c1:09:9c 78:ac:c0:c1:09:56 78:ac:c0:ba:77:e4 78:ac:c0:c1:08:56 78:ac:c0:c1:09:ae 78:ac:c0:ba:78:0b 78:ac:c0:c1:0a:42 78:ac:c0:c2:8b:b5 78:ac:c0:c0:8a:d0 78:ac:c0:c2:8c:cd 78:ac:c0:c0:88:62 78:ac:c0:c1:09:cb 78:ac:c0:c1:0a:87;
+do
+  sudo cobbler system remove --name=$mac
+done
