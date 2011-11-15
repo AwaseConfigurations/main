03be9a84ed56979d72130155065b20716c949b16
Henri Siponen <henri.siponen@gmail.com>
2011-11-15 16:38:21 +0200

    improvements to mkconfp.sh

--- packages/mkconfp.sh
+++ packages/mkconfp.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 # This is a script for building simple configuration packages.
-# It uses DEB_TRANSFORM_FILES and DEB_DIVERT_EXTESION from config-package-dev 
-# to replace config file with a symlink to a modified version of the file.
 # Copyright 2011 Henri Siponen
 # License: GNU General Public License, version 2 or later
 # More help can be found here: http://awaseconfigurations.wordpress.com/2011/10/02/config-package-dev-building-configuration-packages/
@@ -21,62 +19,74 @@ cd "$name"
 mkdir debian
 cd debian
 
-echo "$name (0.1) unstable; urgency=low" >> changelog
-echo "" >> changelog
-echo "  * Initial release." >> changelog
-echo "" >> changelog
-echo " -- Henri Siponen <hng.siponen@gmail.com>  $(date -R)" >> changelog
+cat > changelog << EOF
+$name (0.1) unstable; urgency=low
+
+  * Initial release.
+
+ -- Henri Siponen <hng.siponen@gmail.com>  $(date -R)
+EOF
 
 echo "5" >> compat
 
-echo "Source: $name" >> control
-echo "Section: config" >> control
-echo "Priority: extra" >> control
-echo "Maintainer: Henri Siponen <hng.siponen@gmail.com>" >> control
-echo "Build-Depends: cdbs (>= 0.4.23-1.1), debhelper (>= 4.2.0), config-package-dev (>= 4.5~)" >> control
-echo "Standards-Version: 3.8.0" >> control
-echo "" >> control
-echo "Package: $name" >> control
-echo "Architecture: all" >> control
-echo "Depends: cdbs, \${misc:Depends}" >> control
-echo "Provides: \${diverted-files}" >> control
-echo "Conflicts: \${diverted-files}" >> control
-echo "Description: This is a config package" >> control
+cat > control << EOF
+Source: $name
+Section: config
+Priority: extra
+Maintainer: Henri Siponen <hng.siponen@gmail.com>
+Build-Depends: cdbs (>= 0.4.23-1.1), debhelper (>= 4.2.0), config-package-dev (>= 4.5~)
+Standards-Version: 3.8.0
 
-echo "Source: $name" >> control.in
-echo "Section: config" >> control.in
-echo "Priority: extra" >> control.in
-echo "Maintainer: Henri Siponen <hng.siponen@gmail.com>" >> control.in
-echo "Build-Depends: @cdbs@" >> control.in
-echo "Standards-Version: 3.8.0" >> control.in
-echo "" >> control.in
-echo "Package: $name" >> control.in
-echo "Architecture: all" >> control.in
-echo "Depends: cdbs, \${misc:Depends}" >> control.in
-echo "Provides: \${diverted-files}" >> control.in
-echo "Conflicts: \${diverted-files}" >> control.in
-echo "Description: This is a config package" >> control.in
+Package: $name
+Architecture: all
+Depends: cdbs, \${misc:Depends}
+Provides: \${diverted-files}
+Conflicts: \${diverted-files}
+Description: This is a config package
+EOF
 
-echo "Copyright 2011 Henri Siponen" >> copyright
-echo "License: GNU General Public License, version 2 or later" >> copyright
+cat > control.in << EOF
+Source: $name
+Section: config
+Priority: extra
+Maintainer: Henri Siponen <hng.siponen@gmail.com>
+Build-Depends: @cdbs@
+Standards-Version: 3.8.0
+
+Package: $name
+Architecture: all
+Depends: cdbs, \${misc:Depends}
+Provides: \${diverted-files}
+Conflicts: \${diverted-files}
+Description: This is a config package
+EOF
+
+cat > copyright << EOF
+Copyright 2011 Henri Siponen
+License: GNU General Public License, version 2 or later
+EOF
 
 echo "Type the path to the config file(eg. /etc/deluser.conf): "
 read path
 
-echo "#!/usr/bin/make -f" >> rules
-echo "" >> rules
-echo "DEB_DIVERT_EXTENSION = .hng" >> rules
-echo "DEB_TRANSFORM_FILES_$name += $path.hng" >> rules
-echo "" >> rules
-echo "include /usr/share/cdbs/1/rules/debhelper.mk" >> rules
-echo "include /usr/share/cdbs/1/rules/config-package.mk" >> rules
+cat > rules << EOF
+#!/usr/bin/make -f
+
+DEB_DIVERT_EXTENSION = .hng
+DEB_TRANSFORM_FILES_$name += $path.hng
+
+include /usr/share/cdbs/1/rules/debhelper.mk
+include /usr/share/cdbs/1/rules/config-package.mk
+EOF
 
 echo "Type the name of the config file(eg. deluser.conf): "
 read file
 
-echo "#!/usr/bin/perl -0p" >> transform_"$file".hng
-echo "s/^# (.*bonobo.*)/\$1/m;" >> transform_"$file".hng
-echo "s/^(mango.*banana)/# \$1/m;" >> transform_"$file".hng
+cat > transform_"$file".hng << EOF
+#!/usr/bin/perl -0p
+s/^# (.*bonobo.*)/\$1/m;
+s/^(mango.*banana)/# \$1/m;
+EOF
 
 echo ""
 echo "New config package $name created."
