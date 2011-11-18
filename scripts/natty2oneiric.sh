3c8a8022600be1171031511440080bc5897191e9
Henri Siponen <henri.siponen@gmail.com>
2011-11-18 15:26:34 +0200

    added script for release upgrade

---
+++ scripts/natty2oneiric.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script will automatically upgrade Ubuntu 11.04 to Ubuntu 11.10
+#
+# Copyright 2011 Henri Siponen
+# License: GNU General Public License, version 2 or later
+
+wget https://github.com/AwaseConfigurations/main/raw/master/packages/apt/oneiric-sources/oneiric-sources_0.1_all.deb
+sudo apt-get install gdebi
+sudo gdebi -n oneiric-sources_0.1_all.deb
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
+sudo apt-get -y remove oneiric-sources
+sudo cp /etc/apt/sources.list.backup /etc/apt/sources.list
+sudo apt-get update
+sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y
+sudo reboot
+
