8a00f426f0bc8b519b7bc06df0e070b969e50a47
Henri Siponen <henri.siponen@gmail.com>
2011-11-18 15:40:52 +0200

    small improvements

--- scripts/natty2oneiric.sh
+++ scripts/natty2oneiric.sh
@@ -11,6 +11,8 @@ sudo gdebi -n oneiric-sources_0.1_all.deb
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
 sudo apt-get -y remove oneiric-sources
 sudo cp /etc/apt/sources.list.backup /etc/apt/sources.list
+sudo rm /etc/apt/sources.list.backup
+sudo rm /etc/apt/sources.list.hng
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y
 sudo reboot
