2880c07633cfe8d515276812fba0f8463dcdb936
Henri <hng.siponen@gmail.com>
2011-11-28 15:04:05 +0200

    small improvements

--- fabric/fabfile.py
+++ fabric/fabfile.py
@@ -6,8 +6,8 @@ import string
 
 env.user = 'ubuntu'
 env.password = 'ubuntu'
-env.hosts = ['host1.local', 'host2.local', 'host3.local', 'host4.local',  'host6.local', 'host8.local', 'host9.local', 'host10.local', 'host11.local', 'host12.local', 'host13.local', 'host14.local', 'host15.local', 'host16.local', 'host17.local', 'host18.local', 'host19.local', 'host21.local', 'host22.local', 'host23.local', 'host24.local', 'host25.local', 'host26.local', 'host27.local', 'host28.local', 'host29.local', 'host30.local']
-env.roledefs = {'servers' : ['host1.local'],'workstations' : ['host2.local', 'host3.local', 'host4.local', 'host6.local', 'host8.local', 'host9.local', 'host10.local', 'host11.local', 'host12.local', 'host13.local', 'host14.local', 'host15.local', 'host16.local', 'host17.local', 'host18.local', 'host19.local', 'host21.local', 'host22.local', 'host23.local', 'host24.local', 'host25.local', 'host26.local', 'host27.local', 'host28.local', 'host29.local', 'host30.local']}
+env.hosts = ['host1.local', 'host2.local', 'host3.local', 'host4.local', 'host5.local', 'host6.local', 'host8.local', 'host9.local', 'host10.local', 'host11.local', 'host12.local', 'host13.local', 'host14.local', 'host15.local', 'host16.local', 'host17.local', 'host18.local', 'host19.local', 'host20.local', 'host21.local', 'host22.local', 'host23.local', 'host24.local', 'host25.local', 'host26.local', 'host27.local', 'host28.local', 'host29.local', 'host30.local']
+env.roledefs = {'servers' : ['host1.local'],'workstations' : ['host2.local', 'host3.local', 'host4.local', 'host5.local', 'host6.local', 'host8.local', 'host9.local', 'host10.local', 'host11.local', 'host12.local', 'host13.local', 'host14.local', 'host15.local', 'host16.local', 'host17.local', 'host18.local', 'host19.local', 'host20.local','host21.local', 'host22.local', 'host23.local', 'host24.local', 'host25.local', 'host26.local', 'host27.local', 'host28.local', 'host29.local', 'host30.local']}
 
 @task
 @hosts('host1.local')
@@ -37,7 +37,7 @@ def main():
                 set_bg("awasebg.jpg")
                 local("wget http://myy.haaga-helia.fi/~a0903751/heitero.ogg")
                 set_soundgreeting("heitero.ogg")
-		run("setxkbmap fi")
+		run('setxkbmap fi')
                 reboot()    
 
 @task
@@ -58,7 +58,7 @@ def cmds(command):
 @parallel
 def install(package):
     """Install a package"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("apt-get update")
             for retry in range(2):
@@ -71,7 +71,7 @@ def install(package):
 @parallel
 def install_auto(package):
     """Install a package answering yes to all questions"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("apt-get update")
             sudo('DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get install -o Dpkg::Options::="--force-confold" --force-yes -y %s' % package)
@@ -80,32 +80,35 @@ def install_auto(package):
 @hosts('host1.local')
 def install_apache():
     """Install Apache server with userdir enabled"""
-    if is_host_up(env.host):
-        sudo("apt-get update")
-        sudo("apt-get -y install apache2")
-        if run("ls /etc/apache2/mods-enabled/userdir.conf").failed:
-            sudo("a2enmod userdir")
-            sudo("/etc/init.d/apache2 restart")
+    with settings(linewise=True, warn_only=True):
+    	if is_host_up(env.host):
+            sudo("apt-get update")
+            sudo("apt-get -y install apache2")
+            if run("ls /etc/apache2/mods-enabled/userdir.conf").failed:
+            	sudo("a2enmod userdir")
+            	sudo("/etc/init.d/apache2 restart")
 
 @task
 @parallel
 def uninstall(package):
     """Uninstall a package"""
-    if is_host_up(env.host):
-        sudo("apt-get -y remove %s" % package)
+    with settings(linewise=True, warn_only=True):
+        if is_host_up(env.host):
+            sudo("apt-get -y remove %s" % package)
 
 @task
 @parallel
 def update():
     """Update apt-get"""
-    if is_host_up(env.host):
-        sudo("apt-get update")
+    with settings(linewise=True, warn_only=True):
+        if is_host_up(env.host):
+            sudo("apt-get update")
 
 @task
 @parallel
 def upgrade():
     """pass a sudo command to the hosts"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("apt-get update")
             sudo("apt-get -y upgrade")
@@ -114,7 +117,7 @@ def upgrade():
 @parallel
 def upgrade_auto():
     """Update apt-get and Upgrade apt-get answering yes to all questions"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("apt-get update")
             sudo('apt-get upgrade -o Dpkg::Options::="--force-confold" --force-yes -y')
@@ -122,18 +125,19 @@ def upgrade_auto():
 @task
 @parallel
 @roles('workstations')
-def upgrade_distribution(codename="oneiric"):
-    """Upgrade Ubuntu distribution DEFAULTS TO: oneiric"""
-    with settings(warn_only=True):
+def upgrade_to_oneiric():
+    """Upgrade Ubuntu distribution to oneiric"""
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
-            if run('lsb_release -c') == 'Codename:    %s' % codename:
-                return
-            configure_sources(codename)
-            sudo("apt-get update")
-            sudo('DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y')
-            set_bg()
-            set_soundgreeting()
-            sudo("reboot")
+            if not run('lsb_release -c') == 'Codename:    oneiric':
+	         install_auto('oneiric-sources')
+                 sudo("apt-get update")
+                 sudo('DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y')
+                 local("wget http://myy.haaga-helia.fi/~a0900094/awasebg.jpg")
+                 set_bg("awasebg.jpg")
+                 local("wget http://myy.haaga-helia.fi/~a0903751/heitero.ogg")
+                 set_soundgreeting("heitero.ogg")
+                 sudo("reboot")
 
 @task
 @parallel
@@ -162,16 +166,17 @@ def user_passwd(user, passwd=False):
 @parallel
 def user_delete(user):
     """Delete user"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("deluser %s" % user)
 
 @task
 def status():
     """Display host status"""
-    if is_host_up(env.host):
-        run("uptime")
-        run("uname -a")
+    with settings(linewise=True,warn_only=True):
+    	if is_host_up(env.host):
+     	    run("uptime")
+            run("uname -a")
 
 @task
 @parallel
@@ -190,21 +195,21 @@ def reboot():
 @task
 def file_put(localpath, remotepath):
     """Put file from local path to remote path"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             put(localpath,remotepath)
 
 @task
 def file_get(remotepath, localpath):
     """Get file from remote path to local path"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             get(remotepath,localpath+'.'+env.host)
 
 @task
 def file_remove(remotepath):
     """Remove file at remote path"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             sudo("rm -r %s" % remotepath)
 
@@ -212,7 +217,7 @@ def file_remove(remotepath):
 @parallel
 def configure_reposource():
     """Adds host1.local to repository list"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             with cd("/etc/apt/sources.list.d/"):
                 sudo("echo deb http://host1.local/~ubuntu/ natty main >> repository.list")
@@ -220,22 +225,12 @@ def configure_reposource():
                 sudo("sort -u repository.list > repository.list.new")
                 sudo("cat repository.list.new > repository.list")
                 sudo("rm repository.list.new")
-
-@task
-@parallel
-def configure_sources(sources="oneiric"):
-    """Switch apt sources to different distribution"""
-    with settings(warn_only=True):
-        if is_host_up(env.host):
-            sudo("mv /etc/apt/apt.conf /etc/apt/simo.hng")
-            install_auto('%s-sources' % sources)
-            sudo("mv /etc/apt/simo.hng /etc/apt/apt.conf")
         
 @task
 @parallel
 def set_bg(bg):
     """Set an image file to be used as background"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             if local("ls %s" % bg).failed:
                 local("echo image file not found")
@@ -246,7 +241,7 @@ def set_bg(bg):
 @parallel
 def set_soundgreeting(soundfile):
     """Set a sound file to be used on login"""
-    with settings(warn_only=True):
+    with settings(linewise=True, warn_only=True):
         if is_host_up(env.host):
             if local("ls %s" % soundfile).failed:
                 local("echo sound file not found")
@@ -254,44 +249,33 @@ def set_soundgreeting(soundfile):
                 put(soundfile,"/usr/share/sounds/ubuntu/stereo/dialog-question.ogg",use_sudo=True)
 
 @hosts('host1.local')
-def configure_php():
-    with settings(warn_only=True):
-        if is_host_up(env.host):
-            sudo("mv /etc/apt/apt.conf /etc/apt/simo.hng")
-            install_auto('php-enable-users')
-            sudo("mv /etc/apt/simo.hng /etc/apt/apt.conf")
-
-@hosts('host1.local')
 def setup_webserver():
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            install_apache()
-            install('php5')
-            setup_reprepro()
-            clonegit()
-            configure_reposource()
-            add_to_repo()
-            if run('ls /etc/apache2/mods-enabled/php5.conf.hng').failed:
-                configure_php()
-                sudo("/etc/init.d/apache2 restart")
-            run('mkdir backup')
+    	install_apache()
+        install('php5')
+        setup_reprepro()
+        clonegit()
+        configure_reposource()
+        add_to_repo()
+        if run('ls /etc/apache2/mods-enabled/php5.conf.hng').failed:
+             install_auto('php-enable-users')
+             sudo("/etc/init.d/apache2 restart")
+        run('mkdir backup')
 
 @hosts('host1.local')
 def clonegit():
     """Clones awaseConfigurations git source"""
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            install('git')
-            with cd('~/'):
-                run("git clone https://github.com/AwaseConfigurations/main")
+    	install('git')
+	with cd('~/'):
+        	run("git clone https://github.com/AwaseConfigurations/main")
 
 @hosts('host1.local')
 def add_to_repo():
     """Add sources to reprepro"""
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            with cd('~/public_html/'):
-                run("cp ~/main/packages/php/php-enable-users/php-enable-users_0.1_all.deb ~/public_html/")
+	with cd('~/public_html/'):
+	        run("cp ~/main/packages/php/php-enable-users/php-enable-users_0.1_all.deb ~/public_html/")
                 run("cp ~/main/packages/apt/add-unimulti/add-unimulti_0.1_all.deb ~/public_html/")
                 run("cp ~/main/packages/apt/oneiric-sources/oneiric-sources_0.1_all.deb ~/public_html/")
                 run("reprepro includedeb natty add-unimulti_0.1_all.deb")
@@ -310,9 +294,8 @@ def add_to_repo():
 def setup_squid():
     """Setups Squid for host1.local"""
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            if run("ls /etc/squid/squid.conf").failed:
-                sudo("apt-get update")
+    	if run("ls /etc/squid/squid.conf").failed:
+        	sudo("apt-get update")
                 sudo("apt-get -y install squid")
                 run("wget https://raw.github.com/AwaseConfigurations/main/master/squid/squid.conf")
                 sudo("cp squid.conf /etc/squid/squid.conf")
@@ -323,9 +306,8 @@ def setup_squid():
 def setup_reprepro():
     """Setups reprepro for host1.local"""
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            if run("ls ~/public_html/conf/").failed:
-                sudo("apt-get update")
+    	if run("ls ~/public_html/conf/").failed:
+ 		sudo("apt-get update")
                 sudo("apt-get -y install reprepro")    
                 run("wget https://raw.github.com/AwaseConfigurations/main/master/scripts/reprepro_setup.sh")
                 run("chmod +x reprepro_setup.sh")
@@ -335,8 +317,7 @@ def setup_reprepro():
 @parallel
 def point_to_proxy():
     with settings(warn_only=True):
-        if is_host_up(env.host):
-            sudo("""echo 'Acquire { Retries "0"; HTTP { Proxy "http://host1.local:3128"; }; };' | tee /etc/apt/apt.conf""")
+    	sudo("""echo 'Acquire { Retries "0"; HTTP { Proxy "http://host1.local:3128"; }; };' | tee /etc/apt/apt.conf""")
 
 def generate_passwd(length = 10):
     return ''.join(choice(string.ascii_letters + string.digits) for _ in range(length))
@@ -359,6 +340,7 @@ def is_host_up(host):
 @task
 @parallel
 def smartmon_setup():
+	"""Install and set up smartmontools"""
 	with settings(warn_only=True):
         	if is_host_up(env.host):
 			if run('ls /etc/smartmontools').failed:
@@ -367,7 +349,8 @@ def smartmon_setup():
 
 @task
 def smartmon():
-	 with settings(warn_only=True):
-                if is_host_up(env.host):
+	"""SMART overall-health self-assesment test using smartmontools"""
+	with settings(hide('running', 'user'), warn_only=True):
+	        if is_host_up(env.host):
 			smartmon_setup()
-			sudo('smartctl -H /dev/sda')
+			sudo('smartctl -H /dev/sda | grep "test result"')
