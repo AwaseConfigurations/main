f594910bd9096f66492d16503339a1526f3e6832
Henri Siponen <henri.siponen@gmail.com>
2011-12-13 10:36:20 +0200

    removed use of config package

--- scripts/natty2oneiric.sh
+++ scripts/natty2oneiric.sh
@@ -5,14 +5,7 @@
 # Copyright 2011 Henri Siponen
 # License: GNU General Public License, version 2 or later
 
-wget https://github.com/AwaseConfigurations/main/raw/master/packages/apt/oneiric-sources/oneiric-sources_0.1_all.deb
-sudo apt-get -y install gdebi
-sudo gdebi -n oneiric-sources_0.1_all.deb
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.backup
-sudo apt-get -y remove oneiric-sources
-sudo cp /etc/apt/sources.list.backup /etc/apt/sources.list
-sudo rm /etc/apt/sources.list.backup
-sudo rm /etc/apt/sources.list.hng
+sudo sed -i 's/natty/oneiric/' /etc/apt/sources.list
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y
 sudo reboot
