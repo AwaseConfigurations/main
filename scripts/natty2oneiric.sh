73376d91590e32c6d3b2f18d1cb04c45c04769e7
adamwitherspoon <adamwitherspoon@gmail.com>
2012-03-16 16:17:56 -0700

    Stop puppet from running, Global replace inserted

--- scripts/natty2oneiric.sh
+++ scripts/natty2oneiric.sh
@@ -5,7 +5,8 @@
 # Copyright 2011 Henri Siponen
 # License: GNU General Public License, version 2 or later
 
-sudo sed -i 's/natty/oneiric/' /etc/apt/sources.list
+sudo /etc/init.d/puppet stop
+sudo sed -i 's/natty/oneiric/g' /etc/apt/sources.list
 sudo apt-get update
 sudo DEBIAN_FRONTEND=noninteractive /usr/bin/apt-get dist-upgrade -o Dpkg::Options::="--force-confold" --force-yes -y
 sudo reboot
